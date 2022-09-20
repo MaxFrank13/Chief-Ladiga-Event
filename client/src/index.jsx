8dea009f2f4cbcc6523d396b9e71ecea911be499
Max <maxfrank13@gmail.com>
2022-09-19 23:28:22 -0400

    added days

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import EventDetails from './pages/EventDetails';
+import './index.css';
+// TODO: import React router
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Chief Ladiga</h1>
+    <EventDetails />
   </React.StrictMode>
 );
---
+++ client/src/pages/EventDetails/EventDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Thursday from './components/Thursday';
+import Friday from './components/Friday';
+import Saturday from './components/Saturday';
+import Sunday from './components/Sunday';
+
+export default function EventDetails() {
+  return (
+    <>
+      <Thursday />
+      <Friday />
+      <Saturday />
+      <Sunday />
+    </>
+  )
+}
