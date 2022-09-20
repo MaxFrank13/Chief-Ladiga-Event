1ad211ba1d41c0ea02d2a45c78b1e757bfb20ef2
Max <maxfrank13@gmail.com>
2022-09-19 23:27:09 -0400

    added days

---
+++ client/src/pages/EventDetails/components/Friday.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Friday() {
+  return (
+    <h1 className='text-green-900'>Friday</h1>
+  )
+}
---
+++ client/src/pages/EventDetails/components/Saturday.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Saturday() {
+  return (
+    <h1 className='text-green-900'>Saturday</h1>
+  )
+}
